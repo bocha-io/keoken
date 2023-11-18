f9a33fa9a1e984667813098803f6e991aefd9fd0
hanchon <guillermo.paoletti@gmail.com>
2023-11-18 13:47:03 +0300

    feat: add tailwind support

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -67,143 +67,30 @@ export async function getMetamaskAddress() {
 }
 
 export const App = () => {
-  const {
-    network: { tables, useStore },
-    systemCalls: { addTask, toggleTask, deleteTask },
-  } = useMUD();
-
-  const tasks = useStore((state) => {
-    const records = Object.values(state.getRecords(tables.Tasks));
-    records.sort((a, b) => Number(a.value.createdAt - b.value.createdAt));
-    return records;
-  });
-
   return (
     <>
-      <table>
-        <tbody>
-          {tasks.map((task) => (
-            <tr key={task.id}>
-              <td align="right">
-                <input
-                  type="checkbox"
-                  checked={task.value.completedAt > 0n}
-                  title={
-                    task.value.completedAt === 0n
-                      ? "Mark task as completed"
-                      : "Mark task as incomplete"
-                  }
-                  onChange={async (event) => {
-                    event.preventDefault();
-                    const checkbox = event.currentTarget;
+      <div className="text-4xl">Hola</div>
 
-                    checkbox.disabled = true;
-                    try {
-                      await toggleTask(task.key.key);
-                    } finally {
-                      checkbox.disabled = false;
-                    }
-                  }}
-                />
-              </td>
-              <td>
-                {task.value.completedAt > 0n ? (
-                  <s>{task.value.description}</s>
-                ) : (
-                  <>{task.value.description}</>
-                )}
-              </td>
-              <td align="right">
-                <button
-                  type="button"
-                  title="Delete task"
-                  style={styleUnset}
-                  onClick={async (event) => {
-                    event.preventDefault();
-                    if (
-                      !window.confirm(
-                        "Are you sure you want to delete this task?",
-                      )
-                    )
-                      return;
-
-                    const button = event.currentTarget;
-                    button.disabled = true;
-                    try {
-                      await deleteTask(task.key.key);
-                    } finally {
-                      button.disabled = false;
-                    }
-                  }}
-                >
-                  &times;
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td>
-              <input type="checkbox" disabled />
-            </td>
-            <td colSpan={2}>
-              <form
-                onSubmit={async (event) => {
-                  event.preventDefault();
-                  const form = event.currentTarget;
-                  const fieldset = form.querySelector("fieldset");
-                  if (!(fieldset instanceof HTMLFieldSetElement)) return;
-
-                  const formData = new FormData(form);
-                  const desc = formData.get("description");
-                  if (typeof desc !== "string") return;
-
-                  fieldset.disabled = true;
-                  try {
-                    await addTask(desc);
-                    form.reset();
-                  } finally {
-                    fieldset.disabled = false;
-                  }
-                }}
-              >
-                <fieldset style={styleUnset}>
-                  <input type="text" name="description" />{" "}
-                  <button type="submit" title="Add task">
-                    Add
-                  </button>
-                  <button
-                    onClick={async (e) => {
-                      e.preventDefault();
-                      let address = await getMetamaskAddress();
-                      console.log(address);
-                      const signer = new BrowserProvider(
-                        window.ethereum,
-                      ).getSigner();
-                      let awaitedSigner = await signer;
-                      console.log(signer);
-                      let c = new Contract(
-                        "0x6e9474e9c83676b9a71133ff96db43e7aa0a4342",
-                        abi,
-                        awaitedSigner,
-                      );
-                      console.log(c);
-                      let tx = c.claim(
-                        "0x6e9474e9c83676b9a71133ff96db43e7aa0a4342",
-                        1,
-                      );
-                      console.log(tx);
-                    }}
-                  >
-                    try mm
-                  </button>
-                </fieldset>
-              </form>
-            </td>
-          </tr>
-        </tfoot>
-      </table>
+      <button
+        onClick={async (e) => {
+          e.preventDefault();
+          let address = await getMetamaskAddress();
+          console.log(address);
+          const signer = new BrowserProvider(window.ethereum).getSigner();
+          let awaitedSigner = await signer;
+          console.log(signer);
+          let c = new Contract(
+            "0x6e9474e9c83676b9a71133ff96db43e7aa0a4342",
+            abi,
+            awaitedSigner,
+          );
+          console.log(c);
+          let tx = c.claim("0x6e9474e9c83676b9a71133ff96db43e7aa0a4342", 1);
+          console.log(tx);
+        }}
+      >
+        try mm
+      </button>
     </>
   );
 };
---
+++ packages/client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
